a1869775b1afbde3d6df8e02251ebb520bda9bca
etanb <etan.berkowitz@gmail.com>
2024-10-17 17:13:40 -0700

    swap out mailchimp for mailerlite

--- src/App.js
+++ src/App.js
@@ -8,80 +8,112 @@ import Events from './Components/Events';
 import Music from './Components/Music';
 
 import { useEffect, useState } from 'react';
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
+    let PROJECT_ID = process.env.REACT_APP_SANITY_PROJECT_ID;
+    let DATASET = 'production';
+    let showQUERY = encodeURIComponent('*[_type == "show"]');
+    let showURL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${showQUERY}`;
+    let bandQUERY = encodeURIComponent('*[_type == "band"]');
+    let bandURL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${bandQUERY}`;
+    let beholdURL = process.env.REACT_APP_BEHOLD_URL;
 
-  let PROJECT_ID = process.env.REACT_APP_SANITY_PROJECT_ID;
-  let DATASET = "production";
-  let showQUERY = encodeURIComponent('*[_type == "show"]');
-  let showURL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${showQUERY}`;
-  let bandQUERY = encodeURIComponent('*[_type == "band"]');
-  let bandURL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${bandQUERY}`;
-  let beholdURL = process.env.REACT_APP_BEHOLD_URL
+    const [sanityLoaded, setSanityLoaded] = useState(false);
+    const [shows, setShows] = useState([]);
+    const [bands, setBands] = useState([]);
+    const [behold, setBehold] = useState({});
 
-  const [sanityLoaded, setSanityLoaded] = useState(false)
-  const [shows, setShows] = useState([])
-  const [bands, setBands] = useState([])
-  const [behold, setBehold] = useState({})
+    // initial page load
+    useEffect(() => {
+        fetch(beholdURL).then((res) => {
+            if (res.ok) {
+                res.json().then((res) => {
+                    setBehold(res);
+                });
+            }
+        });
+        fetch(showURL).then((res) => {
+            if (res.ok) {
+                res.json().then((res) => {
+                    setShows(res.result);
+                });
+            }
+        });
+        fetch(bandURL).then((res) => {
+            if (res.ok) {
+                res.json().then((res) => {
+                    setBands(res.result);
+                    setSanityLoaded(true);
+                });
+            }
+        });
+    }, []);
 
-  // initial page load
-  useEffect(() => {
-    fetch(beholdURL).then((res) => {
-      if (res.ok) {
-        res.json().then((res) => {
-          setBehold(res)
-        })
-      }
-    })
-    fetch(showURL).then((res) => {
-      if (res.ok) {
-        res.json().then((res) => {
-          setShows(res.result)
-        })
-      }
-    })
-    fetch(bandURL).then((res) => {
-      if (res.ok) {
-        res.json().then((res) => {
-          setBands(res.result)
-          setSanityLoaded(true)
-        })
-      }
-    })
-  }, [])
+    // subtract number of milliseconds in 7 hours (time conversion)
+    const currentDate = Date.now() - 25200000;
 
-  // subtract number of milliseconds in 7 hours (time conversion)
-  const currentDate = Date.now() - 25200000
+    // sorting shows by date, oldest to newest
+    const sortedShows = [...shows].sort(
+        (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
 
-  // sorting shows by date, oldest to newest
-  const sortedShows = [...shows].sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
+    // every show that happens after the current datetime
+    const futureShows = sortedShows.filter(
+        (show) => Date.parse(show.date) >= currentDate
+    );
+    // every that that has happened before the current datetime
+    const pastShows = sortedShows
+        .filter((show) => Date.parse(show.date) < currentDate)
+        .sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
 
-  // every show that happens after the current datetime
-  const futureShows = sortedShows.filter((show) => Date.parse(show.date) >= currentDate)
-  // every that that has happened before the current datetime
-  const pastShows = sortedShows.filter((show) => Date.parse(show.date) < currentDate).sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
-
-  return (
-    <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="/"
-          element={<Homepage shows={shows} bands={bands} currentDate={currentDate} sortedShows={sortedShows} futureShows={futureShows} sanityLoaded={sanityLoaded} behold={behold} />}
-        />
-        <Route path="/about"
-          element={<About />}
-        />
-        <Route path="/events"
-          element={<Events shows={shows} bands={bands} sanityLoaded={sanityLoaded} currentDate={currentDate} sortedShows={sortedShows} futureShows={futureShows} pastShows={pastShows} />}
-        />
-         <Route path="/music"
-          element={<Music shows={shows} bands={bands} sanityLoaded={sanityLoaded} />}
-        />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <NavBar />
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <Homepage
+                            shows={shows}
+                            bands={bands}
+                            currentDate={currentDate}
+                            sortedShows={sortedShows}
+                            futureShows={futureShows}
+                            sanityLoaded={sanityLoaded}
+                            behold={behold}
+                        />
+                    }
+                />
+                <Route path='/about' element={<About />} />
+                <Route
+                    path='/events'
+                    element={
+                        <Events
+                            shows={shows}
+                            bands={bands}
+                            sanityLoaded={sanityLoaded}
+                            currentDate={currentDate}
+                            sortedShows={sortedShows}
+                            futureShows={futureShows}
+                            pastShows={pastShows}
+                        />
+                    }
+                />
+                <Route
+                    path='/music'
+                    element={
+                        <Music
+                            shows={shows}
+                            bands={bands}
+                            sanityLoaded={sanityLoaded}
+                        />
+                    }
+                />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,37 +1,43 @@
-import "../CSS/Homepage.css"
-import Peg from "../Images/peg-at-woodland.webp"
+import '../CSS/Homepage.css';
+import Peg from '../Images/peg-at-woodland.webp';
 
-import BigEvent from "./BigEvent";
-import MailingList from "./MailingList";
-import IGGallery from "./IGGallery";
-import HomepageEvents from "./HomepageEvents";
+import BigEvent from './BigEvent';
+import MailingList from './MailingList';
+import IGGallery from './IGGallery';
+import HomepageEvents from './HomepageEvents';
 
-import MailchimpSubscribe from "react-mailchimp-subscribe";
-
-function Homepage({ shows, bands, currentDate, sortedShows, futureShows, sanityLoaded, behold }) {
-
-  return (
-    <main>
-      <h2>
-        Woodland Theater is a multi-use creative complex making space for
-        artists in Seattle.
-      </h2>
-      <section id='homepage-top'>
-        <img id='peg-at-woodland' src={Peg} alt='Peg performing at the Woodland Theater' loading='lazy' />
-        <HomepageEvents futureShows={futureShows} bands={bands} sanityLoaded={sanityLoaded}/>
-      </section>
-      <IGGallery behold={behold} sanityLoaded={sanityLoaded} />
-      <MailchimpSubscribe
-        url={process.env.REACT_APP_MAILCHIMP_URL}
-        render={({ subscribe, status, message }) => (
-          <MailingList
-            status={status}
-            message={message}
-            onSubmitted={formData => subscribe(formData)} />
-        )}
-      />
-    </main>
-  );
+function Homepage({
+    shows,
+    bands,
+    currentDate,
+    sortedShows,
+    futureShows,
+    sanityLoaded,
+    behold,
+}) {
+    return (
+        <main>
+            <h2>
+                Woodland Theater is a multi-use creative complex making space
+                for artists in Seattle.
+            </h2>
+            <section id='homepage-top'>
+                <img
+                    id='peg-at-woodland'
+                    src={Peg}
+                    alt='Peg performing at the Woodland Theater'
+                    loading='lazy'
+                />
+                <HomepageEvents
+                    futureShows={futureShows}
+                    bands={bands}
+                    sanityLoaded={sanityLoaded}
+                />
+            </section>
+            <IGGallery behold={behold} sanityLoaded={sanityLoaded} />
+            <MailingList />
+        </main>
+    );
 }
 
 export default Homepage;
