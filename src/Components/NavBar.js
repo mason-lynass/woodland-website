0ae3e19cb7bb891b7a1064afaddbe6a671a56977
etanb <etan.berkowitz@gmail.com>
2024-10-18 11:23:47 -0700

    add dedicated contact page

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,33 +1,32 @@
-import { Link } from "react-router-dom"
-// import Logo from "../Images/old-woodland-logo.webp"
-import Logo from "../Images/WoodlandTheater_Logo_200px_height.webp"
-import IG from "../Images/instagram.svg"
+import { Link } from 'react-router-dom';
+import Logo from '../Images/WoodlandTheater_Logo_200px_height.webp';
 
 function NavBar() {
     return (
         <div id='nav-container'>
-            <div id="nav-flex" className="blue">
+            <div id='nav-flex' className='blue'>
                 <div id='nav-links'>
-                    <Link id="about" to="/about">About</Link>
-                    <Link id="music" to="/music">Music</Link>
+                    <Link id='about' to='/about'>
+                        About
+                    </Link>
+                    <Link id='music' to='/music'>
+                        Music
+                    </Link>
+                    <Link id='contact' to='https://subscribepage.io/8lvN24'>
+                        Contact
+                    </Link>
                 </div>
-                {/* <div id='nav-links'>
-                    <Link id="events" to="/events">Events</Link>
-
-                    
-                </div> */}
-                {/* <a id='IGFooter' href='https://www.instagram.com/woodlandtheater/' target='_blank' rel='noopener noreferrer'>
-                    <img src={IG} alt='link to Woodland Theater Instagram' />
-                </a> */}
             </div>
-            <Link id="to-home" to="/">
-                <img src={Logo} alt='to the Woodland home page' aria-label='to the Woodland home page' id="logo" />
+            <Link id='to-home' to='/'>
+                <img
+                    src={Logo}
+                    alt='to the Woodland home page'
+                    aria-label='to the Woodland home page'
+                    id='logo'
+                />
             </Link>
-
         </div>
-
-
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
