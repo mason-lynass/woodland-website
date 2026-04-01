65c3bb440ee1fe06a61705ed5774311297f544fd
James Madson <james@jamesmadson.me>
2026-04-01 15:53:08 -0700

    Shrink logo on scroll, collapse nav height

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,20 +1,25 @@
 import { Link } from 'react-router-dom';
 import Logo from '../Images/WoodlandTheater_Logo_200px_height.webp';
+import { useEffect, useState } from 'react';
 
 function NavBar() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        function handleScroll() {
+            setScrolled(window.scrollY > 60);
+        }
+        window.addEventListener('scroll', handleScroll, { passive: true });
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <div id='nav-container'>
+        <div id='nav-container' className={scrolled ? 'scrolled' : ''}>
             <div id='nav-flex' className='blue'>
                 <div id='nav-links'>
-                    <Link id='about' to='/about'>
-                        About
-                    </Link>
-                    <Link id='events' to='/events'>
-                        Events
-                    </Link>
-                    <Link id='music' to='/music'>
-                        Music
-                    </Link>
+                    <Link id='about' to='/about'>About</Link>
+                    <Link id='events' to='/events'>Events</Link>
+                    <Link id='music' to='/music'>Music</Link>
                 </div>
             </div>
             <Link id='to-home' to='/'>
