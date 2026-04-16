4ce6958e283dd2e275bb2d16c03defeb66bacec2
James Madson <james@jamesmadson.me>
2026-04-15 19:31:49 -0700

    adding mobile nav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,11 @@
-import { Link, NavLink } from 'react-router-dom'; // eslint-disable-line no-unused-vars
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import Logo from '../Images/WoodlandTheater_Logo_200px_height.webp';
 import { useEffect, useState } from 'react';
 
 function NavBar() {
     const [scrolled, setScrolled] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const location = useLocation();
 
     useEffect(() => {
         function handleScroll() {
@@ -13,8 +15,13 @@ function NavBar() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    // Close menu on route change
+    useEffect(() => {
+        setMenuOpen(false);
+    }, [location]);
+
     return (
-        <div id='nav-container' className={scrolled ? 'scrolled' : ''}>
+        <div id='nav-container' className={`${scrolled ? 'scrolled' : ''} ${menuOpen ? 'menu-open' : ''}`}>
             <div id='nav-flex' className='blue'>
                 <div id='nav-links'>
                     <NavLink className="nav-link" to='/'>Home</NavLink>
@@ -22,7 +29,23 @@ function NavBar() {
                     <NavLink className="nav-link" to='/events'>Events</NavLink>
                     <NavLink className="nav-link" to='/music'>Music</NavLink>
                 </div>
+                <button
+                    id='nav-hamburger'
+                    aria-label={menuOpen ? 'Close menu' : 'Open menu'}
+                    onClick={() => setMenuOpen(o => !o)}
+                >
+                    {menuOpen ? '✕' : '☰'}
+                </button>
+                {menuOpen && (
+                    <div id='nav-mobile-menu' className='blue'>
+                        <NavLink className="nav-link" to='/'>Home</NavLink>
+                        <NavLink className="nav-link" to='/about'>About</NavLink>
+                        <NavLink className="nav-link" to='/events'>Events</NavLink>
+                        <NavLink className="nav-link" to='/music'>Music</NavLink>
+                    </div>
+                )}
             </div>
+
             <Link id='to-home' to='/'>
                 <img
                     src={Logo}
